9c14b682afaa1c1fe91af37c73f5fd7078f10dff
Alibi Daurenbekuly <alibi@Alibis-MacBook-Pro.local>
2026-03-09 16:06:06 +0500

    1.3.0

--- src/main/scala/com/example/scala/SimpleKafkaConsumer.scala
+++ src/main/scala/com/example/scala/SimpleKafkaConsumer.scala
@@ -1,28 +1,13 @@
 package com.example.scala
 
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{Dataset, Row}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 object SimpleKafkaConsumer {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder()
-      .appName("SimpleKafkaConsumer")
-      .master("local[*]")
-      .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-      .config("spark.sql.catalog.rest", "org.apache.iceberg.spark.SparkCatalog")
-      .config("spark.sql.catalog.rest.type", "rest")
-      .config("spark.sql.catalog.rest.uri", "http://localhost:8181")
-      .config("spark.sql.catalog.rest.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
-      .config("spark.sql.catalog.rest.warehouse", "s3://warehouse/")
-      .config("spark.sql.catalog.rest.client.region", "us-east-1")
-      .config("spark.sql.catalog.rest.s3.endpoint", "http://localhost:9000")
-      .config("spark.sql.catalog.rest.s3.path-style-access", "true")
-      .config("spark.sql.catalog.rest.s3.access-key-id", "admin")
-      .config("spark.sql.catalog.rest.s3.secret-access-key", "password")
-      .getOrCreate()
+    val spark = SparkSession.getOrCreateDef()
 
     val kafkaBootstrapServers = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
     val topic = sys.env.getOrElse("KAFKA_TOPIC", "my-topic")
---
+++ src/main/scala/com/example/scala/SparkSession.scala
@@ -0,0 +1,23 @@
+package com.example.scala
+
+object SparkSession {
+
+  def getOrCreateDef(): org.apache.spark.sql.SparkSession = synchronized {
+    return org.apache.spark.sql.SparkSession
+      .builder()
+      .appName("SimpleKafkaProducer")
+      .master("local[*]")
+      .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+      .config("spark.sql.catalog.rest", "org.apache.iceberg.spark.SparkCatalog")
+      .config("spark.sql.catalog.rest.type", "rest")
+      .config("spark.sql.catalog.rest.uri", "http://localhost:8181")
+      .config("spark.sql.catalog.rest.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
+      .config("spark.sql.catalog.rest.warehouse", "s3://warehouse/")
+      .config("spark.sql.catalog.rest.client.region", "us-east-1")
+      .config("spark.sql.catalog.rest.s3.endpoint", "http://localhost:9000")
+      .config("spark.sql.catalog.rest.s3.path-style-access", "true")
+      .config("spark.sql.catalog.rest.s3.access-key-id", "admin")
+      .config("spark.sql.catalog.rest.s3.secret-access-key", "password")
+      .getOrCreate()
+  }
+}
